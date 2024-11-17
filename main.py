d252b9492da0a7a34a19b35784afee6c652eb285
miltefar <fortier.morgan@gmail.com>
2024-11-17 12:43:43 -0500

    More commits from the course

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    theGameClock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000000)
         pygame.display.flip()
+        theGameClock.tick(60)
+        dt = theGameClock.tick(60) / 1000
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
